bc272febe1ea97f42ad045477c2fd4509ce2fd61
shutootaki <ootaki200109@gmail.com>
2023-04-08 12:01:22 +0900

    update agent

--- src/agent.ts
+++ src/agent.ts
@@ -1,11 +1,11 @@
 import { initializeAgentExecutor } from "langchain/agents";
-import { OpenAI } from "langchain";
+import { OpenAI, PromptTemplate } from "langchain";
 import { Calculator, SerpAPI } from "langchain/tools";
 
 require("dotenv").config();
 
 export const runAgent = async () => {
-  const llm = new OpenAI({ temperature: 0.9 });
+  const llm = new OpenAI({ temperature: 0 });
 
   const tools = [new SerpAPI(), new Calculator()];
 
@@ -16,13 +16,34 @@ export const runAgent = async () => {
     true
   );
 
-  const firstPrompt = "尾田栄一郎作の漫画ワンピースの最新刊は何巻ですか？";
+  // first
+  const firstPrompt = "尾田栄一郎 ONE PIECE 最新刊の巻数";
   const firrsRes = await executor.call({ input: firstPrompt });
-  console.log("User", firstPrompt);
-  console.log("Agent", firrsRes.output);
+  console.log("User1", firstPrompt);
+  console.log("Agent1", firrsRes.output);
 
-  const secondPrompt = "123456789割る3は?";
-  const secondRes = await executor.call({ input: secondPrompt });
-  console.log("User", secondPrompt);
-  console.log("Agent", secondRes.output);
+  // second
+  const secondPrompt = new PromptTemplate({
+    inputVariables: ["text"],
+    template:
+      "「{text}」Think step-by-step only of the number of the volume from the text here, and output it precisely.",
+  });
+  const secondRes = await llm.call(
+    await secondPrompt.format({ text: firrsRes.output })
+  );
+  console.log("User2", secondPrompt.template);
+  console.log("Agent2", secondRes);
+
+  // third
+  const thirdPrompt = new PromptTemplate({
+    inputVariables: ["volumes"],
+    template: "{volumes}に5を掛けると？",
+  });
+  const thirdRes = await executor.call({
+    input: await thirdPrompt.format({ volumes: secondRes }),
+  });
+  console.log("User3", thirdPrompt.template);
+  console.log("Agent3", thirdRes.output);
 };
+
+// runAgent();
